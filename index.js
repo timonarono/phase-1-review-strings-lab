cb0251275f8be3a6d44619631bff6088eac63dee
timona <ronotimona@gmail.com>
2023-09-24 15:42:33 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Write your code in this file!
+const currentUser = 'Grace Hopper' ;
+// with string concatenation const welcomeMessage = 'Welcome to Flatbook, ' + currentUser + '!' ;
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`
